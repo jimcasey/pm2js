816da3552c89b9037545deda5c4541bc30fdffc8
Jim Casey <jim.casey@gmail.com>
2023-03-12 22:15:39 -0600

    Refactor content definition

--- packages/converter/src/index.ts
+++ packages/converter/src/index.ts
@@ -21,27 +21,49 @@ const createGroupRefs = (schema: Schema, group: string) => {
     .map((name) => ({ $ref: `#/$defs/${name}` }))
 }
 
+const createContentDef = (content: string = '', schema: Schema) => {
+  // https://prosemirror.net/docs/guide/#schema.content_expressions
+  const def: JSONObject = {}
+  const [_, type, repeat] = /^(\w*)(\+|\*)?/.exec(content) || []
+
+  if (
+    type === 'block' ||
+    type === 'inline' ||
+    repeat === '+' ||
+    repeat === '*'
+  ) {
+    def.type = 'array'
+
+    if (type === 'block') {
+      def.items = { anyOf: createGroupRefs(schema, 'block') }
+    } else if (type === 'inline') {
+      def.items = { anyOf: createGroupRefs(schema, 'inline') }
+    } else {
+      def.items = { $ref: `#/$defs/${type}` }
+    }
+
+    if (repeat === '+') {
+      def.minItems = 1
+    }
+  } else {
+    // TODO this isn't used in prosemirror-schema-basic and needs to be tested
+    def.type = type
+  }
+
+  return def
+}
+
 // TODO comment this up
 export const toJSONSchema = (schema: Schema): object => {
   try {
     const jsonSchema = { ...BASE_SCHEMA }
 
-    const blockRefs = createGroupRefs(schema, 'block')
-    const inlineRefs = createGroupRefs(schema, 'inline')
-
-    console.log('---')
     for (const name in schema.nodes) {
       const { spec } = schema.nodes[name]
-      console.log(name, spec.group, spec.content)
+      const refName = `${name}_content`
 
       if (name === 'doc') {
-        // TODO DRY this up
-        jsonSchema.$defs[`${name}_content`] = {
-          type: 'array',
-          items: {
-            anyOf: blockRefs,
-          },
-        }
+        jsonSchema.$defs[refName] = createContentDef(spec.content, schema)
         continue
       }
 
@@ -55,45 +77,15 @@ export const toJSONSchema = (schema: Schema): object => {
       jsonSchema.$defs[`${name}`] = def
 
       if (spec.content) {
-        // see https://prosemirror.net/docs/guide/#schema.content_expressions
-        const refName = `${name}_content`
-        def.properties['content'] = {
+        def.properties.content = {
           $ref: `#/$defs/${refName}`,
         }
 
-        const [_, contentType, contentRepeat] =
-          /^(\w*)(\+|\*)?/.exec(spec.content) || []
-
-        const contentDef: JSONObject = {
-          type: 'array',
+        const contentRef = createContentDef(spec.content, schema)
+        if (contentRef.minItems && contentRef.minItems > 0) {
+          def.required.push('content')
         }
-        jsonSchema.$defs[refName] = contentDef
-
-        const isArray =
-          contentType === 'block' ||
-          contentType === 'inline' ||
-          contentRepeat === '+' ||
-          contentRepeat === '*'
-
-        if (!isArray) {
-          // TODO this isn't used in prosemirror-schema-basic and needs to be tested
-          contentDef.type = contentType
-        } else {
-          contentDef.type = 'array'
-
-          if (contentType === 'block') {
-            contentDef.items = { anyOf: blockRefs }
-          } else if (contentType === 'inline') {
-            contentDef.items = { anyOf: inlineRefs }
-          } else {
-            contentDef.items = { $ref: `#/$defs/${contentType}` }
-          }
-
-          if (contentRepeat === '+') {
-            def.required.push('content')
-            contentDef.minItems = 1
-          }
-        }
+        jsonSchema.$defs[refName] = contentRef
       }
     }
 
